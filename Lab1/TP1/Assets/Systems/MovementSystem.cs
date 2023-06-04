3a346d68f5039ba9089d4cf93416bdab8977116c
Swendart <sophie@curinier.fr>
2023-06-03 23:24:04 -0400

    add static tag and modify movement system to add it

--- Lab1/TP1/Assets/Systems/MovementSystem.cs
+++ Lab1/TP1/Assets/Systems/MovementSystem.cs
@@ -13,11 +13,14 @@ public class MovementSystem : ISystem
 
         World.ForEach<VelocityComponent>((uint entityId, VelocityComponent velocityComponent) =>
         {
-            PositionComponent positionComponent = World.GetComponent<PositionComponent>(entityId);
-            positionComponent.Position.x += velocityComponent.Velocity.x * Time.deltaTime;
-            positionComponent.Position.y += velocityComponent.Velocity.y * Time.deltaTime;
-            World.SetComponentData<PositionComponent>(entityId, positionComponent);
-            manager.UpdateShapePosition(entityId, positionComponent.Position);
+            if (!World.IsEntityTagged<IsStaticTag>(entityId))
+            {
+                PositionComponent positionComponent = World.GetComponent<PositionComponent>(entityId);
+                positionComponent.Position.x += velocityComponent.Velocity.x * Time.deltaTime;
+                positionComponent.Position.y += velocityComponent.Velocity.y * Time.deltaTime;
+                World.SetComponentData<PositionComponent>(entityId, positionComponent);
+                manager.UpdateShapePosition(entityId, positionComponent.Position);
+            }
         });
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # log8715-lab
 Laboratory for the LOG8715 - Video Game Architecture course at Polytechnique Montréal.
+
+## Lab 1 - Implementation ECS
+### Functionnality
+- Circle spawner
+- Circles have velocities and positions
+- Circle can be static
