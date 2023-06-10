544956a04249a68f48b633f8da48698036a3c270
Swendart <sophie@curinier.fr>
2023-06-10 16:02:01 -0400

    add a colorComponent and update ColorSystem

--- Lab1/TP1/Assets/Systems/ColorSystem.cs
+++ Lab1/TP1/Assets/Systems/ColorSystem.cs
@@ -11,14 +11,14 @@ public class ColorSystem : ISystem
     {
         var manager = ECSManager.Instance;
         int explosionSize = manager.Config.explosionSize;
-        Color entityColor ;
+        ColorComponent colorComponent ;
+        colorComponent.Color = Color.white;
         
         World.ForEach<PositionComponent>((uint entityId, PositionComponent positionComponent) =>
         {
             if (World.IsEntityTagged<IsStaticTag>(entityId))
             {
-                entityColor = Color.red;
-                manager.UpdateShapeColor(entityId, entityColor);
+                colorComponent.Color = Color.red;
             }
             else 
             {
@@ -27,13 +27,11 @@ public class ColorSystem : ISystem
                 {
                     if (World.GetComponent<ProtectionComponent>(entityId).ProtectionDuration > 0)
                     {
-                        entityColor = Color.green;
-                        manager.UpdateShapeColor(entityId, entityColor);
+                        colorComponent.Color = Color.green;
                     }
                     else if ((World.GetComponent<ProtectionComponent>(entityId).ProtectionDuration == 0) && (World.GetComponent<ProtectionComponent>(entityId).ProtectionCooldown > 0))
                     {
-                        entityColor = new Color(218f/255f, 247f/255f, 166f/255f);
-                        manager.UpdateShapeColor(entityId, entityColor);
+                        colorComponent.Color = new Color(218f/255f, 247f/255f, 166f/255f);
                     }
                 }
                 else
@@ -41,16 +39,20 @@ public class ColorSystem : ISystem
                     SizeComponent sizeComponent = World.GetComponent<SizeComponent>(entityId);
                     if (sizeComponent.Size + 1 == explosionSize)
                     {
-                        entityColor = new Color(242f/255f, 140f/255f, 40f/255f);
-                        manager.UpdateShapeColor(entityId, entityColor);
+                        colorComponent.Color = new Color(242f/255f, 140f/255f, 40f/255f);
+                    }
+                    else if (World.IsEntityTagged<IsClickedTag>(entityId))
+                    {
+                        colorComponent.Color = Color.magenta;
                     }
                     else
                     {
-                        entityColor = Color.blue;
-                        manager.UpdateShapeColor(entityId, entityColor);
+                        colorComponent.Color = Color.blue;
                     }
                 }
             }
+            manager.UpdateShapeColor(entityId, colorComponent.Color);
+            World.SetComponentData<ColorComponent>(entityId, colorComponent);
         });
     }
 }
